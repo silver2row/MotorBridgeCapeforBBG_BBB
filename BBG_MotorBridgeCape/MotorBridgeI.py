95ba59a7a0f187c3f294cc3a7bb2e8fa4a6ff4f2
Seth <silver1row@cox.net>
2018-03-01 20:55:43 -0600

    Updating and Kudos!
    Just giving mvduin credit for assistance on this issue.

--- BBG_MotorBridgeCape/MotorBridgeI.py
+++ BBG_MotorBridgeCape/MotorBridgeI.py
@@ -30,11 +30,11 @@
 # FileName : MotorBridge.py
 # by Jiankai.li
 
-import Adafruit_GPIO.I2C as I2C
+import Adafruit_GPIO.I2C as I2C #https://github.com/Seeed-Studio/MotorBridgeCapeforBBG_BBB/issues/6
 import Adafruit_GPIO as GPIO
 import time
 Reset = "P9_23"
-MotorBridge = I2C.Device(0x4b, 2)
+MotorBridge = I2C.Device(0x4b, 2) #same here
 GPIO.setup(Reset, GPIO.OUT)
 
 ReadMode  = 0
